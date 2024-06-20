15f5a25b76d6fa19cae8886f823b3e64dab14f11
Sironysos <les.reseaux.d.alice@gmail.com>
2024-06-20 17:40:22 +0200

    Create table RennesInput and attempt to convert the NbPersBoarding column from varchar to float
    Co-authored-by: Raphaël Dubuget <Marethyu377@users.noreply.github.com>

--- Rennes/Chapter1.sql
+++ Rennes/Chapter1.sql
@@ -1,7 +1,8 @@
 CREATE EXTENSION mobilitydb CASCADE;
 
-set datestyle to [DMY]
+set datestyle to DMY;
 
+DROP TABLE RennesInput;
 CREATE TABLE RennesInput(
   DateFreq date,
   TimeSlot15mn time,
@@ -10,6 +11,16 @@ CREATE TABLE RennesInput(
   TownName varchar(50),
   LineID integer,
   LineName varchar(50),
-  Direction varchar(10),
-  NbPersBoarding float
+  Direction varchar(20),
+  NbPersBoarding varchar(50)
 );
+
+COPY RennesInput(DateFreq, TimeSlot15mn, Timeo, StopName, TownName, LineID, LineName, Direction, NbPersBoarding)
+FROM '/home/raph/Documents/Apprentissage/Stage/Rennes/Data/Freq_Mars2024_data.csv' DELIMITER ';' CSV HEADER;
+
+SELECT * FROM RennesInput LIMIT 30;
+
+UPDATE RennesInput 
+SET NbPersBoarding =  REPLACE(NbPersBoarding , ',', '.');
+UPDATE RennesInput 
+SET NbPersBoarding =  CAST(NbPersBoarding AS float);
