52d0c541af91698c272f887e59aec485fd1b05cf
Sironysos <les.reseaux.d.alice@gmail.com>
2024-06-21 12:43:46 +0200

    Chapter 3 finnished

--- Workshop/Chapter 3/opensky.sql
+++ Workshop/Chapter 3/opensky.sql
@@ -1,5 +1,5 @@
 CREATE EXTENSION MobilityDB CASCADE;
-
+DROP TABLE flights CASCADE;
 CREATE TABLE flights(
     et              bigint,
     icao24          varchar(20),
@@ -33,4 +33,44 @@ UPDATE flights
         lastposupdate_ts = to_timestamp(lastposupdate),
         lastcontact_ts = to_timestamp(lastcontact);
 
+SELECT pg_size_pretty( pg_total_relation_size('flights') );
 
+WITH icao24_with_null_lat AS (
+    SELECT icao24, COUNT(lat)
+    FROM flights
+    GROUP BY icao24
+    HAVING COUNT(lat) = 0
+      )
+DELETE
+FROM flights
+WHERE icao24 IN
+(SELECT icao24 FROM icao24_with_null_lat);
+
+ALTER TABLE flights
+    ADD COLUMN geom geometry(Point, 4326);
+
+UPDATE flights SET
+  geom = ST_SetSRID( ST_MakePoint( lon, lat ), 4326);
+
+CREATE INDEX icao24_time_index
+    ON flights (icao24, et_ts);
+
+CREATE TABLE airframe_traj(icao24, trip, velocity, heading, vertrate, callsign, squawk,
+                           geoaltitude) AS
+SELECT icao24,
+       tgeompointSeq(array_agg(tgeompoint(geom, et_ts) ORDER BY et_ts)
+                      FILTER (WHERE geom IS NOT NULL)),
+       tfloatSeq(array_agg(tfloat(velocity, et_ts) ORDER BY et_ts)
+                  FILTER (WHERE velocity IS NOT NULL)),
+       tfloatSeq(array_agg(tfloat(heading, et_ts) ORDER BY et_ts)
+                  FILTER (WHERE heading IS NOT NULL)),
+       tfloatSeq(array_agg(tfloat(vertrate, et_ts) ORDER BY et_ts)
+                  FILTER (WHERE vertrate IS NOT NULL)),
+       ttextSeq(array_agg(ttext(callsign, et_ts) ORDER BY et_ts)
+                 FILTER (WHERE callsign IS NOT NULL)),
+       tintSeq(array_agg(tint(squawk, et_ts) ORDER BY et_ts)
+                FILTER (WHERE squawk IS NOT NULL)),
+       tfloatSeq(array_agg(tfloat(geoaltitude, et_ts) ORDER BY et_ts)
+                  FILTER (WHERE geoaltitude IS NOT NULL))
+FROM flights
+GROUP BY icao24;
