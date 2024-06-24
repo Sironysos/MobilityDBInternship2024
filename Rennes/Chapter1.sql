15f34bcb2ccdd62efe547c2673bbceca709e55a9
Sironysos <les.reseaux.d.alice@gmail.com>
2024-06-24 11:10:18 +0200

    float conversion nbpersboarding column

--- Rennes/Chapter1.sql
+++ Rennes/Chapter1.sql
@@ -1,6 +1,7 @@
 CREATE EXTENSION mobilitydb CASCADE;
 
 set datestyle to DMY;
+set lc_numeric to 'fr_FR.UTF-8';
 
 DROP TABLE RennesInput;
 CREATE TABLE RennesInput(
@@ -16,22 +17,21 @@ CREATE TABLE RennesInput(
 );
 
 COPY RennesInput(DateFreq, TimeSlot15mn, Timeo, StopName, TownName, LineID, LineName, Direction, NbPersBoarding)
-FROM '/home/raph/Documents/Apprentissage/Stage/Rennes/Data/Freq_Mars2024_data.csv' DELIMITER ';' CSV HEADER;
+FROM '/home/alice/Documents/Stage/MobilityDBInternship2024/Rennes/data/Freq_Mars2024_data.csv' DELIMITER ';' CSV HEADER;
+
+psql -d workshop -c "\copy RennesInput(DateFreq, TimeSlot15mn, Timeo, StopName, TownName, LineID, LineName, Direction, NbPersBoarding) FROM '/home/alice/Documents/Stage/MobilityDBInternship2024/Rennes/data/Freq_Mars2024_data.csv' DELIMITER  ';' CSV HEADER;"
 
 SELECT * FROM RennesInput LIMIT 30;
 
 UPDATE RennesInput 
 SET NbPersBoarding =  REPLACE(NbPersBoarding , ',', '.');
-UPDATE RennesInput 
-SET NbPersBoarding =  CAST(NbPersBoarding AS float);
 
 /* To test */
 ALTER TABLE RennesInput
     ADD COLUMN NbPersBoarding_num float;
 UPDATE RennesInput
     SET NbPersBoarding_num = CAST(NbPersBoarding AS float);
-/* or */
-UPDATE RennesInput
-    SET NbPersBoarding_num = to_float(NbPersBoarding);
 
+ALTER TABLE RennesInput DROP COLUMN nbpersboarding;
 
+SELECT * FROM RennesInput LIMIT 30;
